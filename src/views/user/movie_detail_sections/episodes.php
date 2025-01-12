f78d551121650b691162135a2633ebf2f95ca87e
Ramadhan Nkuutu <78150076+trueRama@users.noreply.github.com>
2025-01-12 11:09:44 +0300

    KASEENEMA UI FIX

--- src/views/user/movie_detail_sections/episodes.php
+++ src/views/user/movie_detail_sections/episodes.php
@@ -1,7 +1,7 @@
 <div class="col-lg-2 col-md-3 col-sm-6 mt-2" xmlns="http://www.w3.org/1999/html">
     <div class="d-flex">
         <form method="post" action="/detail?movie=<?= $move_code; ?>">
-            <h6>Episode 0</h6>
+            <h6>Episode 1</h6>
             <img class="img-sm rounded-10" src="<?php echo $cover_image; ?>" alt="profile">
             <input type="hidden" name="episode_url" value="">
             <button type="submit" name="play_episode" class="btn btn-outline-secondary btn-rounded btn-icon">
@@ -14,7 +14,7 @@
 $sql_pgs_movie = "SELECT * FROM movie_episodes WHERE movie_code = '$move_code' order by id, season ASC";
 $query_pgs_movie = mysqli_query($conn, $sql_pgs_movie);
 $u_check_pgs_movie = mysqli_num_rows($query_pgs_movie);
-$has = 1;
+$has = 2;
 if($u_check_pgs_movie > 0){
     while ($row_movie = mysqli_fetch_array($query_pgs_movie, MYSQLI_ASSOC)){
         $has++;
